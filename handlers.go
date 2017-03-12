58308c65e23994194710f61e0bdfb829c197b22d
Isaac Springer <idspringer@onyxhat.local>
2017-03-11 21:25:48 -0600

    add mux handlers

--- handlers.go
+++ handlers.go
@@ -1,1 +1,9 @@
 package main
+
+import (
+	"net/http"
+)
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write(getData())
+}
--- neatometer.go
+++ neatometer.go
@@ -1,9 +1,31 @@
 package main
 
 import (
-	"fmt"
+	log "github.com/Sirupsen/logrus"
+	"github.com/gorilla/mux"
+	"github.com/kardianos/osext"
+	config "github.com/spf13/viper"
+	"net/http"
 )
 
+func init() {
+	exePath, err := osext.ExecutableFolder()
+	if err != nil {
+		exePath = ".\\"
+	}
+
+	config.AddConfigPath(exePath)
+	config.SetConfigName("config")
+	config.ReadInConfig()
+
+	config.SetDefault("Binding", "0.0.0.0:8080")
+}
+
 func main() {
-	fmt.Println(string(getData()))
+	log.Info("Listening at http://" + config.GetString("Binding"))
+
+	mx := mux.NewRouter()
+	mx.HandleFunc("/", IndexHandler)
+
+	http.ListenAndServe(config.GetString("Binding"), mx)
 }
